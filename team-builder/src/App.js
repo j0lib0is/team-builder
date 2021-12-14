c37720b56787b71e94c80ef9bf284926bd8fd5b1
jacobjolibois <jacobjolibois@hey.com>
2021-12-14 14:44:09 -0600

    built out the form component

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,60 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/form';
+
+const defaultValues = {
+  name: '',
+  hero: ''
+}
 
 function App() {
+
+  // Set up state to hold list of team members
+  const [ team, setTeam ] = useState([
+    { id: 0, name: 'Tony Stark', hero: 'Iron Man' },
+    { id: 1, name: 'Buck Rogers', hero: 'Captain America' },
+    { id: 2, name: 'Bruce Banner', hero: 'Hulk' },
+    { id: 3, name: 'Clint Barton', hero: 'Hawk Eye' },
+    { id: 4, name: 'Natasha Romanov', hero: 'Black Widow' },
+    { id: 5, name: 'Peter Parker', hero: 'SpiderMan' }
+  ]);
+  // Set up state to hold form values and initialize to default values
+  const [ formValues, setFormValues ] = useState(defaultValues);
+  // Set up state to hold an error message
+  const [ error, setError ] = useState('');
+
+  const changeHandler = (inputName, inputValue) => {
+    setFormValues({ ...formValues, [inputName]: inputValue })
+  }
+
+  const submitHandler = () => {
+    // Save the newly added Avenger to a variable
+    const submittedAvenger = {
+      name: formValues.name.trim(),
+      hero: formValues.hero.trim()
+    }
+
+    if (!submittedAvenger.name || !submittedAvenger.hero) {
+      setError('Oops, looks like you are missing something.')
+    } else {
+      setTeam([submittedAvenger, ...team]);
+      setFormValues(defaultValues);
+    }
+
+    console.log(team);
+  }
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        <h1>Avengers</h1>
       </header>
+      <Form avenger={formValues} changeHandler={changeHandler} submitHandler={submitHandler} />
+      <p>{error}</p>
+      {/* {team.map(avenger => {
+        (<div><p>{avenger}</p></div>)
+      })} */}
     </div>
   );
 }
--- team-builder/src/components/form.js
+++ team-builder/src/components/form.js
@@ -1,5 +1,43 @@
 import React from 'react';
 
-export default function FriendForm(props) {
-	const {  } = props;
+export default function Form(props) {
+	const { avenger, submitHandler, changeHandler } = props;
+
+	const onChange = event => {
+		// console.log(event.target.name, event.target.value);
+		const { name, value } = event.target;
+		changeHandler(name, value);
+	}
+
+	const onSubmit = event => {
+		// console.log(event);
+		event.preventDefault();
+    	submitHandler();
+	}
+
+	return(
+		<form onSubmit={onSubmit}>
+			<label>
+				Name
+				<input 
+					value={avenger.name}
+					name='name'
+					type='text'
+					placeholder='Name'
+					onChange={onChange}
+				/>
+			</label>
+			<label>
+				Superhero Name
+				<input 
+					value={avenger.hero}
+					name='hero'
+					type='text'
+					placeholder='Superhero Name'
+					onChange={onChange}
+				/>
+			</label>
+			<button>Assemble!</button>
+		</form>
+	)
 }
